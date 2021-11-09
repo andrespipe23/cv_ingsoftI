a2b306ad1d440b19c788cc0ac9c36362b1b45041
Andres <andresfelipe23072000@gmail.com>
2021-11-09 18:35:16 -0500

    Versión 1

--- src/cv_ejercicio/cv_ejercicio.java
+++ src/cv_ejercicio/cv_ejercicio.java
@@ -16,6 +16,7 @@ public class cv_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("versión 0");
+        System.out.println("versión 1");
     }
     
 }
